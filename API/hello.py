c761606bb8095a0812fd3a541919c152635d8cfe
r-Prabhat <prabhat.lnct16@gmail.com>
2023-11-07 21:17:52 +0530

    flask

--- API/hello.py
+++ API/hello.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/<name>")
 def hello_world(name):
-    return "<p>Hello, World</p>" + name
-@app.route("/<a>/<b>")
-def sumOfTwoNumber(a, b):
-    return str(int(a)+int(b))
+    return "<p>Hello, World</p>"
+# @app.route("/<a>/<b>")
+# def sumOfTwoNumber(a, b):
+#     return str(int(a)+int(b))
