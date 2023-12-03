91bdad291e0be7d595ae7d579fca4ee56dec0dc1
Prabhat Ranjan <100119348+r-Prabhat@users.noreply.github.com>
2023-12-03 11:57:54 +0530

    digit_prediction.py

--- API/digit_prediction.py
+++ API/digit_prediction.py
@@ -1,12 +1,10 @@
-import numpy as np
+
 from flask import Flask, request, jsonify
+import numpy as np
 from sklearn import svm
 from joblib import load
-app = Flask(__name__)
 
-# @app.route("/<name>")
-# def hello_world(name):
-#     return "<p>Hello, World</p>"
+app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def compare_digits():
@@ -43,16 +41,10 @@ def predict_digit(image):
 @app.route("/compare", methods=["POST"])
 def digits_prediction():
     js = request.get_json()
-    img_1 = js["input1"]
-    img_1 = [float(i) for i in img_1]
-    img_2 = js["input2"]
-    img_2 = [float(i) for i in img_2]
+    Image_a = [float(i) for i in js["input1"]]
+    Image_b = [float(i) for i in js["input2"]]
     model = load("models/svm_gamma:0.0005_C:10.joblib")
-    import numpy as np
-    img_1 = np.array(img_1).reshape(-1, 64)
-    img_2 = np.array(img_2).reshape(-1, 64)
-    pred_1 = model.predict(img_1)
-    pred_2 = model.predict(img_2)
-    if pred_1 == pred_2:
-        return "TRUE"
-    return "FALSE"
+    pred_1 = model.predict(np.array(Image_a).reshape(-1, 64))
+    pred_2 = model.predict(np.array(Image_b).reshape(-1, 64))
+    return str(pred_1 == pred_2):
+        
